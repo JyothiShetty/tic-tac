7f01adbf4e349232dfb2c18e9c46b3efa894dbb7
jyothi <jyothibr42@gamil.com>
2022-09-09 16:56:51 +0530

    login validate

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -9,7 +10,7 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 export class RegisterComponent implements OnInit {
 
   public singupForm !: FormGroup;
-  constructor(private formBuilder : FormBuilder, private http : HttpClient) { }
+  constructor(private formBuilder : FormBuilder, private http : HttpClient, private router:Router) { }
 
   ngOnInit(): void {
     this.singupForm = this.formBuilder.group({
@@ -20,7 +21,14 @@ export class RegisterComponent implements OnInit {
     })
   }
   signUp() {
-
+    this.http.post<any>("http://localhost:3000/signupUsers",this.singupForm.value)
+    .subscribe(res=>{
+      alert("Signup Succesfull");
+      this.singupForm.reset();
+      this.router.navigate(['login']);
+    },err=>{
+      alert("something went wrong")
+    })
   }
 
 }
