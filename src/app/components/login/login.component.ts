03ac374b37582aefe5acf63c3754665e2a32149c
jyothi <jyothibr42@gamil.com>
2022-09-11 15:47:56 +0530

    authontication done

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -6,35 +6,52 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-
-  public loginForm! : FormGroup
-  constructor(private formBuilder : FormBuilder, private http: HttpClient, private router: Router) { }
+  public loginForm!: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient,
+    private router: Router
+  ) {}
+  errormsg = '';
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
-      email:['', Validators.required],
-      password:['', Validators.required]
-    })
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+    localStorage.setItem('token', 'notoken');
   }
   login() {
-    this.http.get<any>("http://localhost:3000/signupUsers")
-    .subscribe(res=>{
-      const user = res.find((a:any)=>{
-        return a.email === this.loginForm.value.email && a.password === this.loginForm.value.password
+    const baseUrl = 'http://127.0.0.1:8000/auth/api-token-auth/';
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        username: this.loginForm.value.username,
+        password: this.loginForm.value.password,
+      }),
+    };
+    let token;
+    console.log('this is request opt');
+    console.log(requestOptions);
+    console.log('end');
+    fetch(baseUrl, requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data['token']);
+        if (typeof data['token'] === 'undefined') {
+          console.log('auth failed');
+          this.errormsg = 'Please try again';
+          this.loginForm.reset();
+        } else {
+          console.log('auth success');
+          localStorage.setItem('token', data['token']);
+          console.log('from local storage - ', localStorage.getItem('token'));
+          this.router.navigate(['dashboard']);
+        }
       });
-      if(user){
-        alert("Login Success");
-        this.loginForm.reset();
-        this.router.navigate(['dashboard']);
-      }else{
-        alert("User not found");
-      }
-    },err=>{
-      alert("Something went wrong");
-    })
   }
-
 }
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -5,30 +5,41 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-
-  public singupForm !: FormGroup;
-  constructor(private formBuilder : FormBuilder, private http : HttpClient, private router:Router) { }
+  public singupForm!: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.singupForm = this.formBuilder.group({
-      username:[''],
-      email:[''],
-      password:[''],
-      conPassword:['']
-    })
+      username: [''],
+      email: [''],
+      password: [''],
+      conPassword: [''],
+    });
   }
   signUp() {
-    this.http.post<any>("http://localhost:3000/signupUsers",this.singupForm.value)
-    .subscribe(res=>{
-      alert("Signup Succesfull");
-      this.singupForm.reset();
-      this.router.navigate(['login']);
-    },err=>{
-      alert("something went wrong")
-    })
+    this.http
+      .post<any>(
+        'http://127.0.0.1:8000/auth/create_auth/',
+        this.singupForm.value
+      )
+      // console.log(this.singupForm.value);
+      .subscribe(
+        (res) => {
+          alert('Signup Succesfull');
+          console.log(this.singupForm.value);
+          this.singupForm.reset();
+          this.router.navigate(['login']);
+        },
+        (err) => {
+          alert('something went wrong');
+        }
+      );
   }
-
 }
